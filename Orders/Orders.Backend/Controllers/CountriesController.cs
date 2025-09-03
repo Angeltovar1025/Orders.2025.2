be585424b06197616a6b71c998b45bc31fb567d7
Ángel Tovar <159721431+Angeltovar1025@users.noreply.github.com>
2025-09-03 10:51:06 -0500

    Post and Get for countries

---
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")] // [Controller] reemplaza el nombre api/Countries
+public class CountriesController : ControllerBase
+{
+    private readonly DataContext _context; //Propiedad de lectura context
+
+    public CountriesController(DataContext context)
+    {
+        _context = context; //A esa propiedad de lectura, le asigna lo que viene de context que está en el Datacontext de program donde se instanció la conexión al SQLServer
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsyn()
+    {
+        return Ok(await _context.Countries.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsyn(Country country)
+    {
+        _context.Countries.Add(country);
+        await _context.SaveChangesAsync();
+        return Ok(country);
+    }
+}
